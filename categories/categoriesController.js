cd647165a8a8752e4936bc016c55a2079a42fb0b
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-22 15:53:46 -0300

    adição de: nova cateogria

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -2,11 +2,37 @@ const express = require('express');
 
 const ControllerCategories = express.Router();
 
+const Category = require('./Category');
+
+const Slugify = require('slugify'); // biblioteca q transforma uma string em uma slug(string em url)
+
+
 
 ControllerCategories.get('/admin/categories/new',(req,res)=>{
 
     res.render("admin/cadegories/new.ejs");
 });
+
+//rota para salvar a categoria criada no banco  de  dados
+
+ControllerCategories.post("/categories/save", (req,res)=>{
+    var title = req.body.title;
+
+    if(title != undefined)
+    {
+        Category.create(
+            {
+                title : title,
+                slug : Slugify(title)
+            }
+        ).then(res.redirect("/"))
+
+    }else{
+        res.redirect("admin/cadegories/new");
+    }
+
+
+})
 
 
 
