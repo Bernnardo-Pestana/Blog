78fb117d897190d28a4c4a840a2b689d6537d4b8
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-25 16:02:59 -0300

    continuando o artigo

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -2,11 +2,93 @@ const express = require('express');
 
 const ControllerArticles = express.Router();
 
+const Category = require("../categories/Category")
 
-ControllerArticles.get('/articles',(req,res)=>{
+const Article = require("./Article");
 
-    res.send("Rotas anoitadas");
+const Slugify = require('slugify'); // biblioteca q transforma uma string em uma slug(string em url)
+
+
+ControllerArticles.get('/admin/articles',(req,res)=>{
+
+    Article.findAll({
+        include: [ {model: Category}]
+    }).then((articles)=> {
+        res.render("admin/articles/index.ejs",{articles : articles});
+    })
+
+   
 });
+
+ControllerArticles.get('/admin/articles/new',(req,res)=>{
+
+    Category.findAll().then(categories=>{
+
+
+        res.render("admin/articles/new.ejs",{categories :categories});
+    })
+   
+   
+});
+
+
+
+ControllerArticles.post('/articles/save',(req,res)=>{
+
+    var title = req.body.title;
+    var body = req.body.body;
+    var id = req.body.id; 
+
+    if(title != undefined)
+    {
+        Article.create({
+
+            title : title,
+            body : body,
+            slug :  Slugify(title),
+            categoryId : id
+
+        }).then( (req,res) =>{
+
+            res.redirect("admin/articles/new.ejs");
+
+        })
+    }else{
+        res.redirect("admin/articles/new.ejs");
+    }
+
+       
+});
+
+
+
+ControllerArticles.post("/articles/delete",(req,res)=> //rota para deletar uma categoria. 
+                                                            //Utiliza-se a função destroy do sequelize para deletar um objeto no bd caso os parametros sejam verdadeiros 
+{
+    var id = req.body.id;
+
+    if(id !=undefined)
+    {
+        if(!isNaN(id))
+        {
+          Article.destroy({
+              where: {
+                  id : id 
+              }
+          }).then( ()=>
+          {
+              res.redirect("/admin/articles");
+            })  
+        }else{
+            res.redirect("/admin/articles");
+        }  
+
+    }else{
+        res.redirect("/admin/articles");
+    }
+
+})
+
 
 
 
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('blog','root','Txai1990', {
 
  host:'localhost',
- dialect:'mysql'
+ dialect:'mysql',
+ timezone: '-03:00'
 });
 
 
