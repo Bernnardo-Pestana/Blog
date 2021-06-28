8be8638135d42c95a658ba5877ab3dc74c7bfaff
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-28 19:17:58 -0300

    criação de users com hashcode e salt

--- admin/AdminController.js
+++ admin/AdminController.js
@@ -3,6 +3,8 @@ const express = require('express');
 const ControllerAdmin= express.Router();
 const Admin = require("./Admin");
 
+const bcrypt = require("bcryptjs"); // hash-code e salt para senhas de usuarios
+
 
 ControllerAdmin.get("admin/user",(req,res)=>
 {
@@ -23,6 +25,27 @@ ControllerAdmin.post("/admin/create",(req,res)=>
   var email = req.body.email;
   var senha = req.body.password;
 
+//procura saber se o email ja foi cadastrado
+  Admin.findOne({where : {email : email}}).then(user=>{
+    if(user == undefined){
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password,salt);
+
+      Admin.create({
+        email : email,
+        password : hash
+      }).then(()=>{
+        res.redirect("/");
+      }).catch((err)=>{
+        res.redirect("/");
+      })
+    }else{
+      res.redirect("/admin/user/create");
+    }
+  })
+
+  
     
 })
 
